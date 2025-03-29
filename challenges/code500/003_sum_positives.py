2a60ce40ba9b94522f3490017b4e4b95011640c2
SaicoBys <JacobMoronta0000@gmail.com>
2025-03-29 16:00:52 -0500

    feat: add solution for challenge 003 (sum_positives)

--- challenges/code500/003_sum_positives.py
+++ challenges/code500/003_sum_positives.py
@@ -8,5 +8,12 @@ Entrada: [-1, 2, -3, 4, -5]
 Salida esperada: 6
 """
 # ========================================
+result = 0
+lst = [2, -3, -4, 5, -6, 5]
 
-# ✅ Tu solución aquí...
+for num in lst:         # Recorro cada número en la lista para verificar si es positivo
+    if num < 0:         # Si el número es negativo, lo omito con continue
+        continue
+    else:
+        result += num   # Si el número es positivo, lo sumo a result
+print(result)
